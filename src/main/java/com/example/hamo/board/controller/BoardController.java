d3349bb9521139bb895ae6dd9c1fec7350307c7e
cold-glass <coldglas.1341@gmail.com>
2025-01-06 21:30:27 +0900

    게시글 상세 페이지, 게시즐 작성&수정 페이지, 공지 페이지, 공지 상세보기 페이지, 추가 메핑 작업 25.01.06

--- src/main/java/com/example/hamo/board/controller/BoardController.java
+++ src/main/java/com/example/hamo/board/controller/BoardController.java
@@ -1,5 +1,31 @@
 package com.example.hamo.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
 public class BoardController {
+	@GetMapping("/board/boardDetails")
+	public String boarDetails() {
+		return "board/boardDetails";
+	}
+	
+	@GetMapping("/board/boardInsert")
+	public String boardInsert() {
+		return "board/boardInsert"; 
+	}
+	
+	@GetMapping("board/noticeView")
+	public String noticeView() {
+		return "board/noticeView";
+	}
+	
+	@GetMapping("board/noticeDetail")
+	public String noticeDetail() {
+		return "board/noticeDetail";
+	}
 
 }
