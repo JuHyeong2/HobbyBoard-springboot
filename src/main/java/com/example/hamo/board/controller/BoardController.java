1c040ecc8d097d3866ab2799ba73f7e3d77fbade
seonghyeon hwang <ghkd5370@naver.com>
2025-01-21 18:25:18 +0900

    25/01/21 메인 공지사항 리스트 출력 및 상세보기 맵핑(-ing)

--- src/main/java/com/example/hamo/board/controller/BoardController.java
+++ src/main/java/com/example/hamo/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +13,12 @@ import com.example.hamo.admin.model.service.AdminService;
 import com.example.hamo.admin.model.vo.Admin;
 import com.example.hamo.board.model.service.BoardService;
 import com.example.hamo.board.model.vo.Board;
+import com.example.hamo.common.Pagination;
+import com.example.hamo.common.vo.PageInfo;
+import com.example.hamo.member.model.vo.Member;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 
@@ -48,20 +54,37 @@ public class BoardController {
 		
 	
 	@GetMapping("noticeView")
-	public String noticeView(Model model) {
-		ArrayList<Admin> aList = aService.selectNoticeList();
-		model.addAttribute("aList", aList); 
+	public String noticeView(Model model, @RequestParam(value="page", defaultValue="1")int currentPage,HttpServletRequest request) {
+		int listCount =  aService.getListCount();
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount, 6);
+		ArrayList<Admin> list = aService.selectNoticeList(pi);
+		model.addAttribute("aList", list).addAttribute("pi", pi).addAttribute("loc", request.getRequestURI());
+		
+		System.out.println(list);
+		
 		return "board/noticeView";
 	}
 	
-	@GetMapping("noticeDetail")
-	public String noticeDetail() {
-		return "board/noticeDetail";
+	@GetMapping("/{id}/{page}")
+	public String selectNotice(@PathVariable("id") int bId, @PathVariable("page") int page, HttpSession session, Model model) {
+		Member m = (Member)session.getAttribute("loginUser");
+		Admin a = bService.selectNotice(m, bId);
+		
+		if(a != null) {
+			model.addAttribute("a", a).addAttribute("page", page);
+			return "board/noticeDetail";
+		} else {
+			return null;
+		}
 	}
+	
+	
 	
 	@GetMapping("project")
 	public String project() {
 		return "board/project";
 	}
+	
+	
 
 }
--- src/main/java/com/example/hamo/board/model/service/BoardService.java
+++ src/main/java/com/example/hamo/board/model/service/BoardService.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
+import com.example.hamo.admin.model.vo.Admin;
 import com.example.hamo.board.model.mapper.BoardMapper;
 import com.example.hamo.board.model.vo.Board;
+import com.example.hamo.member.model.vo.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,5 +24,14 @@ public class BoardService {
 	public Board selectBoard(int boardNo) {
 		
 		return mapper.selectBoard(boardNo);
+	}
+
+	 public Admin selectNotice(Member m, int bId) {
+		Admin a = mapper.selectNotice(bId);
+		int result = mapper.updateNoticeCount(bId);
+		if(result >0) {
+			a.setViews(result);
+		}
+		return a;
 	}
 }
