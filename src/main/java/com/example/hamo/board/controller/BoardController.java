644bb00cb0972ff79b1a24f16b5c5870fa025da2
seonghyeon hwang <ghkd5370@naver.com>
2025-01-23 17:01:19 +0900

    25/01/23 추가 수정

--- src/main/java/com/example/hamo/board/controller/BoardController.java
+++ src/main/java/com/example/hamo/board/controller/BoardController.java
@@ -1,7 +1,6 @@
 package com.example.hamo.board.controller;
 
 
-import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.SdkClientException;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.example.hamo.admin.model.service.AdminService;
