347b9f34c5eee59a3839644c53a26fd77a9ed5cb
JuHyeong2 <100174932+JuHyeong2@users.noreply.github.com>
2025-01-24 10:16:09 +0900

    Update EmailCertificationUtil.java 주석제거

--- src/main/java/com/example/hamo/common/util/EmailCertificationUtil.java
+++ src/main/java/com/example/hamo/common/util/EmailCertificationUtil.java
@@ -1,8 +1,8 @@
 package com.example.hamo.common.util;
 
 
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 
