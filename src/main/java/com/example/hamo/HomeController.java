7edb02716ddae84c67aa2f7f392cb4b7268a7a11
HyunJunKang <guswns1185@naver.com>
2025-02-01 19:30:38 +0900

    카테고리, 배너 부분(수정 필요)

--- src/main/java/com/example/hamo/HomeController.java
+++ src/main/java/com/example/hamo/HomeController.java
@@ -1,18 +1,25 @@
 package com.example.hamo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.example.hamo.board.model.service.BoardService;
 import com.example.hamo.board.model.vo.Board;
+import com.example.hamo.board.model.vo.Category;
 import com.example.hamo.board.model.vo.Image;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -26,9 +33,57 @@ public class HomeController {
 	private String bucket;
 	
 	@GetMapping("/")
-	public String main(Model model) {
+	public String main(Model model, HttpServletRequest request) {
+//		String ipAddress = getClientIp(request);
+//		String userAgent = request.getHeader("User-Agent");
 		
+//		HashMap<String, String> map = new HashMap<String, String>();
+//		map.put("ipAddress", ipAddress);
+//		map.put("userAgent", userAgent);
+//		
+//		int result = bService.insertLog(map);
+		
+		
+		
+		ArrayList<Board> banner = bService.getBanner();
+//		for (Board board : banner) {
+//            if (board.getThumbnailUrl() != null) {
+//                board.setThumbnailUrl(amazonS3.getUrl(bucket, board.getThumbnailUrl()).toString());
+//            }
+//        }
+		ArrayList<Image> imgList = bService.selectImageListBoard();
+		for (Board board : banner) {
+			for(Image img : imgList) {
+				if(board.getBoardNo() == img.getBuNo()) {
+					board.setThumbnailUrl(amazonS3.getUrl(bucket, img.getImgRename()).toString());
+					break;
+				}
+			}
+		}		
 		ArrayList<Board> list = bService.selectBoardList();
+		BoardList(list);
+		ArrayList<Category> category = bService.selectCategory();
+		
+		model.addAttribute("list", list);
+		model.addAttribute("category",category);
+		model.addAttribute("banner", banner);
+		
+		return "index";
+	
+	}
+
+	@GetMapping("/category/{categoryNo}")
+	public String boardCategory(@PathVariable("categoryNo") int categoryNo, Model model) {
+		ArrayList<Board> list = bService.boardCategory(categoryNo);
+		BoardList(list);
+		
+		model.addAttribute("list", list);
+		return "index :: #boardGrid";
+	}
+	
+	
+	// 썸네일 이미지 가져오기
+	private void BoardList(ArrayList<Board> list) {
 		ArrayList<Image> imgList = bService.selectImageListBoard();
 		for(Board  board : list) {
 			for(Image img : imgList) {
@@ -38,7 +93,7 @@ public class HomeController {
 				}
 			}
 		}
-		
+		// 참여자 가져오기
 		ArrayList<Board> participants = bService.participantsByBoard();
 		System.out.println(participants);
 		for(Board b : list) {
@@ -49,8 +104,48 @@ public class HomeController {
 			}
 		}
 		
-		model.addAttribute("list", list);
-		
-		return "index";
 	}
+
+	// client 실ip 가져오는 메소드
+//	public static String getClientIp(HttpServletRequest request) {
+//	    String clientIp = null;
+//	    boolean isIpInHeader = false;
+//
+//	    List<String> headerList = new ArrayList<>();
+//	    headerList.add("X-Forwarded-For");
+//	    headerList.add("HTTP_CLIENT_IP");
+//	    headerList.add("HTTP_X_FORWARDED_FOR");
+//	    headerList.add("HTTP_X_FORWARDED");
+//	    headerList.add("HTTP_FORWARDED_FOR");
+//	    headerList.add("HTTP_FORWARDED");
+//	    headerList.add("Proxy-Client-IP");
+//	    headerList.add("WL-Proxy-Client-IP");
+//	    headerList.add("HTTP_VIA");
+//	    headerList.add("IPV6_ADR");
+//
+//	    for (String header : headerList) {
+//	        clientIp = request.getHeader(header);
+//	        if (StringUtils.hasText(clientIp) && !"unknown".equalsIgnoreCase(clientIp)) {
+//	            isIpInHeader = true;
+//	            break;
+//	        }
+//	    }
+//
+//	    if (!isIpInHeader) {
+//	        clientIp = request.getRemoteAddr();
+//	    }
+//	    
+//	    if ("0:0:0:0:0:0:0:1".equals(clientIp) || "127.0.0.1".equals(clientIp)) {
+//	        InetAddress address = null;
+//			try {
+//				address = InetAddress.getLocalHost();
+//			} catch (UnknownHostException e) {
+//				e.printStackTrace();
+//			}
+//	        clientIp = address.getHostAddress();
+//	    }
+//
+//	    
+//	    return clientIp;
+//	}
 }
--- src/main/java/com/example/hamo/board/model/mapper/BoardMapper.java
+++ src/main/java/com/example/hamo/board/model/mapper/BoardMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.example.hamo.admin.model.vo.Admin;
 import com.example.hamo.board.model.vo.Board;
+import com.example.hamo.board.model.vo.Category;
 import com.example.hamo.board.model.vo.Image;
 import com.example.hamo.board.model.vo.Reply;
 
@@ -44,6 +45,14 @@ public interface BoardMapper {
 	int insertParticipant(HashMap<String, Integer> map);
 
 	int selectParticipant(HashMap<String, Integer> map);
+	
+//	int insertLog(HashMap<String, String> map);
+	
+	ArrayList<Category> selectCategory();
+
+	ArrayList<Board> boardCategory(int categoryNo);
+
+	ArrayList<Board> getBanner();
 
 }
 
